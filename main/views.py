e2f4f5820badfa1febf4ff6a427cbb35a1dd6255
cahyacoding <cahya.bgg@gmail.com>
2024-09-04 13:20:43 +0700

    Membenarkan isi views.py

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306275380',
+        'name': 'Cahya Bagus Gautama Gozales ',
+        'class': 'PBP C'
     }
 
     return render(request, "main.html", context)
