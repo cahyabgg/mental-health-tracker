bb58276153cd0551248c1f25f260f4e08b2ddf33
cahyacoding <cahya.bgg@gmail.com>
2024-11-13 13:53:10 +0700

    tutorial 8 selesai

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from main.views import (
     show_main,create_mood_entry,show_xml,
     show_json,show_xml_by_id,show_json_by_id,
     register,login_user,logout_user,edit_mood,
-    delete_mood,add_mood_entry_ajax
+    delete_mood,add_mood_entry_ajax,create_mood_flutter
     )
 
 
@@ -22,4 +22,5 @@ urlpatterns = [
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
     path('delete/<uuid:id>', delete_mood, name='delete_mood'), # sesuaikan dengan nama fungsi yang dibuat
     path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
+    path('create-flutter/', create_mood_flutter, name='create_mood_flutter'),
 ]
